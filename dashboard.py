75ab028e29c04e615f370c60bc592465bf633496
Deyanira Aguilar <kazz.aguilar@gmail.com>
2022-11-26 12:43:46 -0600

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,6 +4,9 @@ import numpy as np
 from PIL import Image
 import urllib.request
 import xlsxwriter
+import requests
+import io
+
 #======================================================================================
 #======================================================================================
 URL = 'https://scontent.fgua1-3.fna.fbcdn.net/v/t1.18169-9/1660963_606924706060130_284056617_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=wnFMucwa0_kAX-Ph1TL&_nc_ht=scontent.fgua1-3.fna&oh=00_AfA8xCmc1P5H30p1a5oBEmHua_2wlhmU_ZU70HojSbDdBg&oe=63976B39'
@@ -24,6 +27,11 @@ st.markdown(hide_st_style, unsafe_allow_html = True)
 st.image("https://i.imgur.com/HZDOve8.jpg")
 #======================================================================================
 #======================================================================================
+url = "https://raw.githubusercontent.com/Kazeazul/Paquetes-de-Software-II/main/Gastos%20CG.csv"
+download = requests.get(url).content
+#======================================================================================
+if 'gastos_cg' not in st.session_state:
+    st.session_state.gastos_cg = pd.read_csv(io.StringIO(download.decode('utf-8')))
 
 st.title("Bienvenido")
-st.write(pd.read_csv("Gastos CG.csv", encoding="utf8"))
+st.write(st.session_state.gastos_cg)
